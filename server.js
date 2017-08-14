d4f711b35795584a90d9f5e494e121ede9c2da1f
RAJESHADHI <arajesh.tvmalai95@gmail.com>
2017-08-14 13:15:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+title:'article-one rajesh',
+heading:'article-one',
+date:'sep 8,2017',
+content:`  
+    <p>
+         
+         This is my first article content.
+           
+    </p> ` 
+    
+};
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date= data.date;
+    var content = data.content;
+var htmlTemplate =`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>    
+        <body>
+        <div class = "container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    <p>
+                        ${content}
+                    </p>
+                </div>
+            </h3>
+         </div>
+        </body>
+
+</html>
+`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(createTemlate(articleOne));
 });
 
 
