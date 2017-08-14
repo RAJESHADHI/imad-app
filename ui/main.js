a0911e5f779c7f2afe1c9ec917ddea54d3fc9e32
RAJESHADHI <arajesh.tvmalai95@gmail.com>
2017-08-14 14:44:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element= document.getElementById('main text');
 element.innerHTML='new value';
 
 var img = document.getElementById('img');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick= function(){
-    img.style.marginleft='800px';
+ var interval = setInterval(moveRight,50);
 };
